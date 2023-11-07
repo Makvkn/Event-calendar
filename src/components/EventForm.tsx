8d67e637ea87c154fa80655e67819dfe0b91a8c8
Makvkn <max.makovkin@yandex.ru>
2023-11-07 22:14:00 +0300

    The end of api with Service

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,32 +1,69 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Button, DatePicker, Form, Input, Row, Select} from "antd";
 import {rules} from "../utils/rules";
+import {IUser} from "../models/IUser";
+import {IEvent} from "../models/IEvent";
+import {Moment} from "moment";
+import {formatDate} from "../utils/date";
+import {useTypedSelector} from "../hooks/useTypedSelector";
 
-const EventForm: FC = () => {
+interface EventFormProps {
+    guests: IUser[];
+    submit: (event: IEvent) => void
+}
+
+const EventForm: FC<EventFormProps> = (props) => {
+    const [event, setEvent] = useState<IEvent>({
+        author: '',
+        guest: '',
+        date: '',
+        description: ''
+    } as IEvent)
+
+    const {user} = useTypedSelector(state => state.auth);
+
+
+    const selectDate = (date: Moment | null) => {
+        if (date) {
+            setEvent({...event, date: formatDate(date.toDate())})
+            console.log(formatDate(date.toDate()));
+        }
+    }
+
+    const submitForm = () => {
+        props.submit({...event, author: user.username})
+    }
+
     return (
-        <Form>
+        <Form onFinish={submitForm}>
             <Form.Item
                 label="Event description"
                 name="description"
                 rules={[rules.required('Please input your event')]}
             >
-                <Input/>
+                <Input
+                    onChange = {(e) => setEvent({...event, description: e.target.value}) }
+                    value = {event.description}
+
+
+                />
             </Form.Item>
             <Form.Item label="Event Date"
                        name="date"
-                       rules={[rules.required('Please input date')]}
+                       rules={[rules.required('Please input date'), rules.isDateAfter("You can't create an action in the past")]}
             >
-                <DatePicker/>
+                <DatePicker
+                    onChange = {(date: any) => selectDate(date)}
+                    />
             </Form.Item>
             <Form.Item>
-                <Select
-                    options={[
-                        {value: 'jack', label: 'Jack'},
-                        {value: 'lucy', label: 'Lucy'},
-                        {value: 'Yiminghe', label: 'yiminghe'},
-                        {value: 'disabled', label: 'Disabled', disabled: true},
-                    ]}
-                />
+                <Select onChange={(guest: string) => setEvent({...event, guest})}>
+                    {props.guests.map(guest =>
+                        <Select.Option key={guest.username} className="Oprions">
+                            {guest.username}
+                        </Select.Option>
+                    )}
+                </Select>
             </Form.Item>
             <Row justify="end">
                 <Form.Item>
--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,7 +1,18 @@
+import moment, {Moment} from "moment";
 
 export const rules = {
     required: (message: string) => ({
         required: true,
         message
+    }),
+    isDateAfter: (message: string) => () => ({
+        validator(_: any, value: Moment) {
+            // debugger;
+            // console.log(value);
+            if (value.isAfter(moment())) {
+                return Promise.resolve()
+            }
+            return Promise.reject(new Error(message));
+        }
     })
 }
